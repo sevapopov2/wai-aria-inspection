84406f63b27df367edf8811c30b9046b3db7eb10
Vsevolod Popov <sevapopov13@gmail.com>
2022-10-06 12:21:26 +0300

    Add generic index view

--- server/apps/main/views.py
+++ server/apps/main/views.py
@@ -1,1 +1,9 @@
 """Views for main project's app."""
+
+from django.views import generic
+
+
+class IndexView(generic.DetailView):
+    """Generic view for index template."""
+
+    template_name = 'main/index.html'
--- server/urls.py
+++ server/urls.py
@@ -1,8 +1,9 @@
 """Global project's URL Configuration."""
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('server.apps.main.urls')),
     path('admin/', admin.site.urls),
 ]
