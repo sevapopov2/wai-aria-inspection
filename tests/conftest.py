79cc8fd6e773e7a3f367afe006847ea6fd508e9c
Vsevolod Popov <sevapopov13@gmail.com>
2022-10-29 20:57:25 +0300

    Change implicitly_wait function to work for 2 seconds

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,8 +11,8 @@ def browser():
     webbrowser = selenium.webdriver.chrome()
     # Maximize browser window
     webbrowser.maximize_window()
-    # Set up calls to wait for 10 seconds
-    webbrowser.implicitly_wait(10)
+    # Set up calls to wait for 2 seconds
+    webbrowser.implicitly_wait(2)
     # Return webdriver instance
     yield webbrowser
     # Quit the webdriver instance
