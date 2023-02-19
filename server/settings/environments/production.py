89009cd65e5012a1b73b5096f6539ca7beae2385
Vsevolod Popov <sevapopov13@gmail.com>
2023-02-19 18:08:31 +0300

    Change ALLOWED-HOSTS format in production environment settings file.

--- server/settings/environments/production.py
+++ server/settings/environments/production.py
@@ -4,7 +4,11 @@ from server.settings.components import config
 
 
 DEBUG = False
-ALLOWED_HOSTS = ('.{}'.format(config('DOMAIN_NAME')),)
+ALLOWED_HOSTS = [
+    config('DOMAIN_NAME'),
+    'localhost',
+]
+
 STATIC_ROOT = '/var/www/django/static'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 MEDIA_ROOT = '/var/www/django/media'
